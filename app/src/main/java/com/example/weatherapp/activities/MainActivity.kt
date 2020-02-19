3fbddd25e2fca63a7488d114cbc8e91d9b956893
Aiman Nabeel <aiman.nabeel@touchsurgery.com>
2020-02-19 13:33:12 +0000

    Adding anko to make api request work

--- app/src/main/java/com/example/weatherapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/activities/MainActivity.kt
@@ -2,54 +2,29 @@ package com.example.weatherapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.R
 import com.example.weatherapp.adapters.ForecastListAdapter
-import com.example.weatherapp.data.ForecastRequest
 import com.example.weatherapp.domain.commands.RequestForecastCommand
-import okhttp3.*
-import java.io.IOException
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    private val client = OkHttpClient()
-    private val RESPONSETAG = "RESPONSE"
-
-    private val items = listOf(
-        "Mon 6/23 - Sunny - 31/17",
-        "Tue 6/24 - Foggy - 21/8",
-        "Wed 6/25 - Cloudy - 22/17",
-        "Thurs 6/26 - Rainy - 18/11",
-        "Fri 6/27 - Foggy - 21/10",
-        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-        "Sun 6/29 - Sunny - 20/7"
-    )
-
-    private val request = RequestForecastCommand("94043").execute()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //run("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=15646a06818f61f7b8d7823ca833e1ce&zip=94043&mode=json&units=metric&cnt=7")
-
-        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        val forecastList = find<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(request)
-    }
 
-    private fun run(url: String) {
-        val request = Request.Builder().url(url).build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                println(response.body()?.string())
-                Log.d(RESPONSETAG,"Response successful")
+        doAsync {
+            val result = RequestForecastCommand("94043").execute()
+            uiThread {
+                forecastList.adapter = ForecastListAdapter(result)
             }
-
-            override fun onFailure(call: Call, e: IOException) {}
-        })
+        }
     }
 }
